76cb80e0b0ef08afa922df2a43b92dd52387aec0
PAVEL KOZIN <sart6zaV@yandex.ru>
2024-03-06 23:50:31 +0300

    Я сдаюсь

--- js/main.js
+++ js/main.js
@@ -1,8 +1,14 @@
 const mobileMenu = document.querySelector('.mobile-menu');
+const header = document.querySelector('.header');
+
+const product = document.querySelector('#product');
+const topsell = document.querySelector('#top_sell');
+const trend = document.querySelector('#trend');
+const new_ = document.querySelector('#new');
+
 document.querySelector('.menu_button').addEventListener('click', (event) => {
   mobileMenu.classList.remove('hidden');
   mobileMenu.classList.add('visible');
-  console.log('fefef');
 });
 
 mobileMenu
@@ -11,3 +17,24 @@ mobileMenu
     mobileMenu.classList.remove('visible');
     mobileMenu.classList.add('hidden');
   });
+
+function scrollToSection(event) {
+  const classes = event.target.className;
+  if (classes.includes('nav_a')) {
+    switch (console.log(content)) {
+      case '#product':
+        link.products.scrollIntoView();
+        mobileMenu.classList.remove('visible');
+        mobileMenu.classList.add('hidden');
+      case 'Products':
+        link.products.scrollIntoView();
+      case 'Blog':
+        link.products.scrollIntoView();
+      case 'new':
+        link.products.scrollIntoView();
+    }
+  }
+}
+
+mobileMenu.addEventListener('click', scrollToSection);
+header.addEventListener('click', scrollToSection);
