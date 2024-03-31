1b35d2e39dfd2fd77915a466a10f45cec410ae0a
PAVEL KOZIN <sart6zaV@yandex.ru>
2024-03-31 23:52:47 +0300

    Переделал карточки

--- src/js/tab.js
+++ src/js/tab.js
@@ -1,24 +1,3 @@
-const cardInfoWatchesList = [
-  {
-    image: 'image 15.png',
-    cardTitle: 'Безпроводные наушники №456',
-    reviews: 100,
-    price: 32,
-  },
-  {
-    image: 'image 16.png',
-    cardTitle: 'Безпроводные наушники №981',
-    reviews: 130,
-    price: 95,
-  },
-  {
-    image: 'image 13.png',
-    cardTitle: 'Безпроводные наушники №330',
-    reviews: 88,
-    price: 1223,
-  },
-];
-
 // "beforebegin" – вставить html непосредственно перед elem,
 // "afterbegin" – вставить html в начало elem,
 // "beforeend" – вставить html в конец elem,
@@ -32,42 +11,59 @@ const cardInfoWatchesList = [
 // mobilemenu.after(div)
 // mobilemenu.replaceWith(div)
 
-const sellersTabCardWrapper = document
-  .querySelector('.top-sell')
-  .querySelectorAll('.topsell_headphoness');
-// // const earphonesTabCardWrapper = document
-// //   .querySelector('.earphones')
-// //   .querySelector('.tab__card-wrapper');
-// export const sellersTabCardWrapper2 = document
-//   .querySelector('.trend')
+// const sellersTabCardWrapper = document
+//   .querySelector('.top-sell')
 //   .querySelectorAll('.topsell_headphoness');
+// // // const earphonesTabCardWrapper = document
+// // //   .querySelector('.earphones')
+// // //   .querySelector('.tab__card-wrapper');
+// // export const sellersTabCardWrapper2 = document
+// //   .querySelector('.trend')
+// //   .querySelectorAll('.topsell_headphoness');
 
-const sellersTabBtnWrapper = document
-  .querySelector('.top-sell')
-  .querySelectorAll('.btns_sell');
+// const sellersTabBtnWrapper = document
+//   .querySelector('.top-sell')
+//   .querySelectorAll('.btns_sell');
 
-let f = sellersTabBtnWrapper[0];
-f.addEventListener('click', (event) => {
-  const target = event.target;
+export default function tabs(tabNames, dataCards, tabContainerElement) {
+  // tabNames - string[]
+  // dataCards - {
+  //     cardTitle: string,
+  //     reviews: number,
+  //     price: number,
+  // }[][]
+  // tabContainerElement - Element | null
 
-  const ovalButtons = f.querySelectorAll('.btn_top_picks');
+  if (!tabContainerElement) {
+    return;
+  }
 
-  ovalButtons.forEach((btn) => {
-    btn.classList.remove('active');
-  });
-  console.log(target.nodeName);
-  if (target.nodeName == 'BUTTON') {
+  const tabCardWrapper = document
+    .querySelector(`${tabContainerElement}`)
+    .querySelector('.topsell_headphoness');
+  const tabBtnWrapper = document
+    .querySelector(`${tabContainerElement}`)
+    .querySelector('.btns_sell');
+
+  tabBtnWrapper.addEventListener('click', (event) => {
+    const target = event.target;
+    const emptyWrapper = document.createElement('div');
+    emptyWrapper.classList.add('.topsell_headphoness');
+
+    const ovalButtons = tabBtnWrapper.querySelectorAll('button');
+    ovalButtons.forEach((btn) => {
+      btn.classList.remove('active');
+    });
     target.classList.add('active');
-    // sellersTabCardWrapper.replaceWith('')
-  }
-  if (target.innerText == 'Watches') {
-    deletetab('top-sell');
-    // getCards(cardInfoTopPicksList, sellersTabCardWrapper[0]);
-  } else {
-    deletetab('top-sell');
-    getCards(cardInfoWatchesList, sellersTabCardWrapper[0]);
-  }
-});
+
+    tabNames.forEach((tab, index) => {
+      if (tab == target.innerText) {
+        deletetab(tabContainerElement);
+        getCards(dataCards[index], tabCardWrapper);
+      }
+    });
+  });
+}
 
 function getCards(cardInfoList, elem) {
   const arr = cardInfoList.forEach((cardInfo) => {
@@ -99,7 +95,7 @@ function getCards(cardInfoList, elem) {
 }
 
 function deletetab(m) {
-  const tabs = document.querySelector(`.${m}`).querySelectorAll('.headphones');
+  const tabs = document.querySelector(`${m}`).querySelectorAll('.headphones');
   // console.log(tabs);
   for (let x of tabs) {
     x.remove();
--- src/main.js
+++ src/main.js
@@ -1,6 +1,27 @@
 import getHeadphones from './js/service/get-headphones.js';
 import { isShowWelcomePopup, showWelcomePopup } from './js/pup.js';
-let module = await import('./js/tab.js');
+
+import tabs from './js/tab.js';
+const cardInfoWatchesList = [
+  {
+    image: 'image 15.png',
+    cardTitle: 'Безпроводные наушники №456',
+    reviews: 100,
+    price: 32,
+  },
+  {
+    image: 'image 16.png',
+    cardTitle: 'Безпроводные наушники №981',
+    reviews: 130,
+    price: 95,
+  },
+  {
+    image: 'image 13.png',
+    cardTitle: 'Безпроводные наушники №330',
+    reviews: 88,
+    price: 1223,
+  },
+];
 
 //import { tab } from './js/tab.js';
 import scss from './scss/main.scss';
@@ -11,11 +32,15 @@ const header = new Header('header');
 header.setTemplate();
 header.createHeader('body');
 menu();
-async function x() {
-  let cardInfoTopPicksList = await getHeadphones();
-  console.log('Вывод после экспорта' + cardInfoTopPicksList);
-}
-x();
+
+const tabSellersNames = ['Top Picks', 'Watches'];
+const tabEarphonesNames = ['Earbuds', 'Wireless', 'Wired'];
+tabs(tabSellersNames, [cardInfoWatchesList, cardInfoWatchesList], '.top-sell');
+tabs(
+  tabEarphonesNames,
+  [cardInfoWatchesList, cardInfoWatchesList, cardInfoWatchesList],
+  '.trend'
+);
 
 //const getCards = require('./js/tab');
 if (!isShowWelcomePopup) {
